82127aea2bd910e282745bab9d2409384b8ef639
Wani-Mukesh <Wani_Mukesh@rediffmail.com>
2021-08-23 01:18:25 +0530

    Added MyFirstJavaProgram

---
+++ MyFirstJavaProject/src/MyFirstJavaProgram.java
@@ -0,0 +1,13 @@
+
+public class MyFirstJavaProgram {
+	public static void main(String... args)
+	{
+		System.out.println("Hello World");
+		for (int i = 0; i < 10; i++) {
+			if(i==4)
+				break;
+			else
+				System.out.println(i);					
+		}		
+	}
+}
